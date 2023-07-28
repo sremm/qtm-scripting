9b0c2897c987251cf63473bfb936a55eff1e1de4
Sten Remmelg <sten.remmelg@qualisys.se>
2023-07-28 17:43:12 +0200

    add sports markerset renaming

--- offenburg_scripts/rename_markers.py
+++ offenburg_scripts/rename_markers.py
@@ -44,6 +44,52 @@ def _rename_markers(marker_mapping):
             print(f"Could not change label from {cur_label} to {new_label}")
             continue
 
+def _rename_markers_sports_markerset():
+    marker_mapping = {
+        "HeadFront": "Q_HeadFront",
+        "HeadR": "Q_HeadR",
+        "HeadL": "Q_HeadL",
+        "RShoulderTop": "Q_RShoulderTop",
+        "LShoulderTop": "Q_LShoulderTop",
+        "Chest": "Q_Chest",
+        "LElbowIn": "Q_LElbowIn",
+        "RElbowIn": "Q_RElbowIn",
+        "LElbowOut": "Q_LElbowOut",
+        "RElbowOut": "Q_RElbowOut",
+        "LWristOut": "Q_LWristOut",
+        "RWristOut": "Q_RWristOut",
+        "LWristIn": "Q_LWristIn",
+        "RWristIn": "Q_RWristIn",
+        "WaistRFront": "Q_WaistRFront",
+        "WaistLFront": "Q_WaistLFront",
+        "RThighFrontLow": "Q_RThighFrontLow",
+        "LThighFrontLow": "Q_LThighFrontLow",
+        "LKneeIn": "Q_LKneeIn",
+        "RKneeIn": "Q_RKneeIn",
+        "LKneeOut": "Q_LKneeOut",
+        "RKneeOut": "Q_RKneeOut",
+        "LShinFrontHigh": "Q_LShinFrontHigh",
+        "RShinFrontHigh": "Q_RShinFrontHigh",
+        "LAnkleOut": "Q_LAnkleOut",
+        "RAnkleOut": "Q_RAnkleOut",
+        "LForefoot5": "Q_LForefoot5",
+        "RForefoot5": "Q_RForefoot5",
+        "LForefoot2": "Q_LForefoot2",
+        "RForefoot2": "Q_RForefoot2",
+        "SpineThoracic2": "Q_SpineThoracic2",
+        "SpineThoracic12": "Q_SpineThoracic12",
+        "RArm": "Q_RArm",
+        "LArm": "Q_LArm",
+        "WaistR": "Q_WaistR",
+        "WaistL": "Q_WaistL",
+        "IOR_LV5": "Q_WaistBack",
+        "RHand2": "Q_RHand2",
+        "LHand2": "Q_LHand2",
+        "RHeelBack": "Q_RHeelBack",
+        "LHeelBack": "Q_LHeelBack",
+    }
+    _rename_markers(marker_mapping)
+
 def _rename_markers_markerset_name_X():
     marker_mapping = {
         "HeadFront": "NewHeadFront",
@@ -69,6 +115,7 @@ def _rename_markers_markerset_name_Z():
 
 
 def _setup_commands():
+    add_command("rename_markers_sports_markerset", _rename_markers_sports_markerset)
     add_command("rename_markers_X", _rename_markers_markerset_name_X)
     add_command("rename_markers_Y", _rename_markers_markerset_name_Y)
     add_command("rename_markers_Z", _rename_markers_markerset_name_Z)
@@ -100,6 +147,7 @@ def add_menu_and_commands():
 
         # setup menu
         menu_id = qtm.gui.insert_menu_submenu(None, "Rename markers menu")
+        add_menu_item(menu_id, "Rename markers (sports markerset)", "rename_markers_sports_markerset")
         add_menu_item(menu_id, "Rename markers (markerset X)", "rename_markers_X") # we can add a button, but if we call the commands from the script, we don't really need to
         add_menu_item(menu_id, "Rename markers (markerset Y)", "rename_markers_Y") 
     except Exception as e:
